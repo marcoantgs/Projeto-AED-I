976a71e159af72ce63e0fade2dfd57df28625b23
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-10-23 19:23:18 -0300

    Clase Pena

---
+++ Pena.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Presidio
+{
+    class Pena
+    {
+        private Data Data_inicial;
+        private Data Data_final;
+        private string Crime;
+
+        public Data getdata_inicial()
+        {
+            return Data_inicial;
+        }
+
+       
+        public Data getdata_final()
+        {
+            return Data_inicial;
+        }
+
+        public void setdata_final(Data df)
+        {
+            Data_inicial = df;
+        }
+
+        public string getCrime()
+        {
+           return Crime;
+        }
+
+            
+        public Pena (  Data inicial, Data final , string crime)
+        {
+            Data_inicial = inicial;
+            Data_final = final;
+            Crime = crime;
+        }
+
+        public Pena()
+        {
+
+        }
+
+        public int Periodo_dias()
+        {
+            return this.Data_inicial.Diferenca_dias(Data_final);
+        }
+
+        public int Periodo_anos()
+        {
+            return this.Data_inicial.Diferenca_anos(Data_final);
+        }
+
+        public override string ToString()
+        {
+            return Crime + " / Tempo de reclusao: " + Periodo_anos() + " anos " + Periodo_dias() + " dias ";
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
